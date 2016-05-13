24831f30b869d774bc1c369f157ccb986a4277d6
Raymond Sak <menghou0924@gmail.com>
2016-05-13 12:03:33 +0800

    add meteor installation

---
+++ meteor.bash
@@ -0,0 +1,14 @@
+# Installation guide for meteor
+
+# 1. install the application
+curl https://install.meteor.com/ | sh
+
+# 2. install node.js and nvm
+brew install node
+
+# 3. for meteor 0.9 before, install meteorite
+npm install -g meteorite
+
+# 4. install the packages at the meteor folder
+mrt install
+
